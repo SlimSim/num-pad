9f65d0319af844e784b3375550a6ceae4ed7e6b7
Slim Sim <SlimSimApps@gmail.com>
2014-11-21 16:28:30 +0100

    fixing value and pocibility to set nr of coulmns

--- components/num-pad.js
+++ components/num-pad.js
@@ -1,5 +1,41 @@
 Polymer("num-pad", {
   ready: function(){
+    this.value = parseInt(this.value) || 5;
+    var aPaperButtons = this.shadowRoot.querySelectorAll('.butt');
+    var select = this.value==0?aPaperButtons.length: this.value;
+    select-=1;
+    aPaperButtons[select].setAttribute('raised', true);
+
+
+
+
+    if(this.columns){
+      switch (parseInt(this.columns)){
+      case 5:
+        this.style.maxWidth = "548px";
+        this.style.minWidth = "458px";
+        break;
+      case 4:
+        this.style.maxWidth = "457px";
+        this.style.minWidth = "366px";
+        this.visualiseDiv('.four');
+        break;
+      case 3:
+        this.style.maxWidth = "365px";
+        this.style.minWidth = "275px";
+        this.visualiseDiv('.three');
+        break;
+      case 2:
+        this.style.maxWidth = "274px";
+        this.style.minWidth = "183px";
+        break;
+      case 1:
+        this.style.maxWidth = "182px";
+        this.style.minWidth = "90px";
+      }
+    }
+
+
 
   },
   select: function(a, b, c){
@@ -8,7 +44,13 @@ Polymer("num-pad", {
       aPaperButtons[i].removeAttribute('raised');
     }
     c.setAttribute('raised', true);
-
+    this.value = parseInt(c.innerText);
+  },
+  visualiseDiv: function(strNumber){
+        var aDiv = this.shadowRoot.querySelectorAll(strNumber);
+        for(var i=aDiv.length; i-->0 ; ){
+          aDiv[i].style.display = "block";
+        }
   }
 });
 
