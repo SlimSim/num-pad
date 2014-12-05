ec28f524f6761472ea576dad26b55d0695390aaa
SlimSim Stationara Jobb <slimsimapps@gmail.com>
2014-12-05 17:26:01 +0100

    fixing external value change, fixing custom special character. cleaning code

--- components/num-pad.js
+++ components/num-pad.js
@@ -1,56 +1,20 @@
 Polymer("num-pad", {
   ready: function(){
-    this.value = parseInt(this.value) || 5;
-    var aPaperButtons = this.shadowRoot.querySelectorAll('.butt');
-    var select = this.value==0?aPaperButtons.length: this.value;
-    select-=1;
-    aPaperButtons[select].setAttribute('raised', true);
-
-
-
-
-    if(this.columns){
-      switch (parseInt(this.columns)){
-      case 5:
-        this.style.maxWidth = "548px";
-        this.style.minWidth = "458px";
-        break;
-      case 4:
-        this.style.maxWidth = "457px";
-        this.style.minWidth = "366px";
-        this.visualiseDiv('.four');
-        break;
-      case 3:
-        this.style.maxWidth = "365px";
-        this.style.minWidth = "275px";
-        this.visualiseDiv('.three');
-        break;
-      case 2:
-        this.style.maxWidth = "274px";
-        this.style.minWidth = "183px";
-        break;
-      case 1:
-        this.style.maxWidth = "182px";
-        this.style.minWidth = "90px";
-      }
-    }
-
-
-
+    this.special = typeof this.special != 'undefined'? this.special: "0";
+    this.value = typeof this.value != 'undefined'? parseInt(this.value): 1;
   },
   select: function(a, b, c){
+    this.value = c.getAttribute('value');
+  },
+  valueChanged: function(){
     var aPaperButtons = this.shadowRoot.querySelectorAll('paper-button')
     for (var i=0; i<aPaperButtons.length; i++){
       aPaperButtons[i].removeAttribute('raised');
+      if(parseInt(aPaperButtons[i].getAttribute('value') ) == this.value){
+        aPaperButtons[i].setAttribute('raised', true);
+      }
     }
-    c.setAttribute('raised', true);
-    this.value = parseInt(c.innerText);
-  },
-  visualiseDiv: function(strNumber){
-        var aDiv = this.shadowRoot.querySelectorAll(strNumber);
-        for(var i=aDiv.length; i-->0 ; ){
-          aDiv[i].style.display = "block";
-        }
   }
 });
+
 
